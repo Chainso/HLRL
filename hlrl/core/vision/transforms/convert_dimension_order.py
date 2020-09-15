8219afcd7251481c46e35f35228e28772650eb24
unknown <chanzobryan@gmail.com>
2020-09-14 23:06:42 -0400

    Fixed view error

--- hlrl/core/vision/transforms/convert_dimension_order.py
+++ hlrl/core/vision/transforms/convert_dimension_order.py
@@ -7,4 +7,4 @@ class ConvertDimensionOrder(Transform):
     Converts a tensor of shape NHWC -> NCHW
     """
     def __call__(self, tensor: Tensor) -> Tensor:
-        return tensor.permute(0, 3, 1, 2)
+        return tensor.permute(0, 3, 1, 2).contiguous()
