5dc19943dbd5928c3ca022ebbdfd4be3f8d22c21
Chainso <chanzobryan@gmail.com>
2020-10-11 16:35:03 -0400

    Using lambda for compose since it is pickleable

--- hlrl/core/common/functional/compose.py
+++ hlrl/core/common/functional/compose.py
@@ -10,24 +10,4 @@ def compose(*functions: Callable):
     Args:
         functions (Callable): A variable number of functions to compose.
     """
-    return functools.reduce(compose2, functions)
-
-def compose2(func1: Callable, func2: Callable):
-    """
-    Composes 2 functions g, f into a single function f(g(*args, **kwargs)).
-
-    Args:
-        func1 (Callable): The outer function of the composition.
-        func2 (Callable): The inner function of the composition.
-    """
-    def function_composition(*args: Any, **kwargs: Any):
-        """
-        Returns the result of the function composition.
-
-        Args:
-            *args (Any): The positional arguments to the inner function.
-            **kwargs (Any): The keyword arguments to the inner function.
-        """
-        return func2(func1(*args, **kwargs))
-
-    return function_composition
+    return functools.reduce(lambda x, f: f(x), reversed(functions))
