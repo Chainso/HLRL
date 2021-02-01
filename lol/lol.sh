e72d1844c55f2cbae50295d1da41b7c707c3b70f
Chainso <chanzobryan@gmail.com>
2021-01-31 23:38:24 -0500

    Can run the lol environment

---
+++ lol/lol.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source env.sh
+python lol.py
---
+++ lol/server.sh
@@ -0,0 +1,3 @@
+cd /home/chanzo/Games/league-sandbox/drive_c/Games/league-sandbox/LeagueSandbox-RL-Learning/GameServerConsole/bin/Debug/netcoreapp3.0
+./GameServerConsole --redis_port 6379
+cd -
