b7cbf349a528b20e4f187e1efd85c4a4d6eb2d9a
unknown <chanzobryan@gmail.com>
2021-03-08 23:27:28 -0500

    Added a function to help initialize biases

--- hlrl/torch/common/functional/initialization.py
+++ hlrl/torch/common/functional/initialization.py
@@ -1,15 +1,18 @@
-import torch
-
 from typing import Callable
 
-def initialize_weights(init_func: Callable[[torch.Tensor], None]):
+import torch
+from torch import nn
+
+def initialize_weights(
+        init_func: Callable[[torch.Tensor], None]
+    ) -> Callable[[nn.Module], None]:
     """
     Returns a function that can be applied to a module to initializes the module
     weights.
 
     Args:
-        init_func (Callable[[torch.Tensor], None]): A function taking a tensor
-            and modifies the tensor's weight data in place.
+        init_func: A function taking a torch module and modifies the module's
+        weight data in place.
     """
     def applied_func(module):
         """
@@ -17,5 +20,25 @@ def initialize_weights(init_func: Callable[[torch.Tensor], None]):
         """
         if hasattr(module, "weight"):
             init_func(module.weight.data)
+
+    return applied_func
+
+def initialize_bias(
+        init_func: Callable[[torch.Tensor], None]
+    ) -> Callable[[nn.Module], None]:
+    """
+    Returns a function that can be applied to a module to initializes the module
+    bias.
+
+    Args:
+        init_func: A function taking a torch module and modifies the module's
+        bias data in place.
+    """
+    def applied_func(module):
+        """
+        The function that is applied to the module to initialize the weights
+        """
+        if hasattr(module, "bias"):
+            init_func(module.bias.data)
 
     return applied_func
