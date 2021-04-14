33124584e92f8bb7433606e65711340babd280ce
Chainso <chanzobryan@gmail.com>
2021-04-14 18:30:29 -0400

    Added normalize reward import

--- hlrl/torch/algos/wrappers/__init__.py
+++ hlrl/torch/algos/wrappers/__init__.py
@@ -1,3 +1,7 @@
-from .rnd import RND
+
 from .noisy import NoisyAlgo
+from .normalize_reward import NormalizeRewardAlgo
 from .recurrent import TorchRecurrentAlgo
+from .rnd import RND
+
+__all__ = ["NoisyAlgo", "NormalizeRewardAlgo", "TorchRecurrentAlgo", "RND"]
