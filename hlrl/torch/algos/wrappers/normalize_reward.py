42d1d909e59b3dbb07da5159cb4cf698a80a9991
Chainso <chanzobryan@gmail.com>
2021-04-14 18:28:16 -0400

    Fixed annotations

--- hlrl/torch/algos/wrappers/normalize_reward.py
+++ hlrl/torch/algos/wrappers/normalize_reward.py
@@ -1,16 +1,17 @@
+from typing import Dict
+
+import torch
 import torch.nn as nn
 
 from hlrl.core.common.wrappers import MethodWrapper
+from hlrl.torch.algos import TorchRLAlgo
 from hlrl.torch.utils.contexts import evaluating, training
 
 class NormalizeRewardAlgo(MethodWrapper):
     """
     A wrapper to normalize rewards for the algorithm.
     """
-    def __init__(
-            self,
-            algo: TorchRLAlgo
-        ):
+    def __init__(self, algo: TorchRLAlgo):
         """
         Creates the wrapper to use reward normalization exploration for the
         algorithm.
@@ -22,23 +23,9 @@ class NormalizeRewardAlgo(MethodWrapper):
 
         self.reward_norm = nn.BatchNorm1d(1)
 
-    def _get_loss(self, states):
-        """
-        Returns the loss of the RND network on the given states.
-        """
-        states = self.normalization_layer(states)
-        rnd_pred = self.rnd(states)
-
-        with torch.no_grad():
-            rnd_target = self.rnd_target(states)
-
-        rnd_loss = self.rnd_loss_func(rnd_pred, rnd_target)
-
-        return rnd_loss
-
     def process_batch(
             self,
-            rollouts: Dict[str, Union[torch.Tensor, np.ndarray]]
+            rollouts: Dict[str, torch.Tensor]
         ) -> Dict[str, torch.Tensor]:
         """
         Processes and normalizes the reward for a batch.
