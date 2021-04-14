a7260cf2e65e9efde23d75e417d85ff3869005f8
Chainso <chanzobryan@gmail.com>
2021-04-14 18:25:38 -0400

    Added training context

--- hlrl/torch/utils/contexts/training.py
+++ hlrl/torch/utils/contexts/training.py
@@ -22,3 +22,24 @@ def evaluate(module: nn.Module):
         # Switch batch to training if needed
         if training:
             module.train()
+
+@contextmanager
+def training(module: nn.Module):
+    """
+    A context manager for training the module.
+
+    Args:
+        module: The module to switch to training in the context.
+
+    Returns:
+        A generator for the context of the module.
+    """
+    training = module.training
+
+    try:
+        module.train()
+        yield module
+    finally:
+        # Switch batch to training if needed
+        if not training:
+            module.eval()
